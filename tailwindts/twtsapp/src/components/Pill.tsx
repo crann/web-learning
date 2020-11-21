c6f85bdbf8d8e4cee170c13e5c688a1884f28a20
Daniel Crann <dancrann@gmail.com>
2020-11-21 16:10:49 +0000

    Tailwind: Updates pill

--- tailwindts/twtsapp/src/components/Pill.tsx
+++ tailwindts/twtsapp/src/components/Pill.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react";
 const Pill: FC<{}> = (props) => {
   return (
     <>
-      <span className="text-xs text-gray-700 border border-gray-500 hover:border-gray-400 rounded p-1 bg-gray-200 hover:bg-gray-100">
+      <span className="text-xs text-gray-700 border border-gray-500 hover:border-gray-400 rounded-full p-1 bg-gray-200 hover:bg-gray-100">
         My Pill
       </span>
     </>
